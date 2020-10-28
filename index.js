dc787835443f341cc494f2a52102831d5667c4a6
Mark Wild <mark.wild189@gmail.com>
2020-10-27 17:01:51 -0700

    commit

--- index.js
+++ index.js
@@ -8,16 +8,19 @@ const CronController = require('./controller/cron.controller');
 const app = express();
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-    res.header('Access-Control-Expose-Headers', 'Content-Length');
-    res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
-    if (req.method === 'OPTIONS') {
-        return res.send(200);
-    } else {
-        return next();
-    }
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Expose-Headers', 'Content-Length');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Accept, Authorization, Content-Type, X-Requested-With, Range'
+  );
+  if (req.method === 'OPTIONS') {
+    return res.send(200);
+  } else {
+    return next();
+  }
 });
 
 app.use(bodyParser.json());
@@ -25,5 +28,5 @@ Router.routesConfig(app);
 CronController.startCronJobs();
 
 app.listen(config.port, function () {
-    console.log('app listening at port %s', config.port);
+  console.log('app listening at port %s', config.port);
 });
