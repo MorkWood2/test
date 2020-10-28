97f35305bb6e834334fd78539874c4ab73f7a100
Mark Wild <mark.wild189@gmail.com>
2020-10-28 09:50:00 -0700

    commit

--- controller/cron.controller.js
+++ controller/cron.controller.js
@@ -2,11 +2,17 @@ const RepositoryController = require('./repository.controller');
 const CronJob = require('cron').CronJob;
 
 function updateDaily() {
-    RepositoryController.updateRepositories();
+  RepositoryController.updateRepositories();
 }
 
 exports.startCronJobs = function () {
-    new CronJob('0 0 * * *', function () {
-        updateDaily()
-    }, null, true, 'UTC');
+  new CronJob(
+    '0 0 * * *',
+    function () {
+      updateDaily();
+    },
+    null,
+    true,
+    'UTC'
+  );
 };
